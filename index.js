d1649db36f8bc8fd693fb68b20a08017eed924f7
Gerard Crockenberg <gmahred@gmail.com>
2020-02-14 14:06:19 -0500

    added heroku functionality

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ Go code!
 
 const server = require("./server");
 
-server.listen(5000, () => {
-  console.log("\n* Server Running on http://localhost:5000 *\n");
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`\n* Server Running on ${port} *\n`);
 })
